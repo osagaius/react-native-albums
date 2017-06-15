bf9f3e815b5b4ec84b2709e93fdf330b4f446523
Osayame Gaius-Obaseki <osayame@gmail.com>
2017-06-14 21:37:45 -0400

    add header component and reference in main app

--- index.ios.js
+++ index.ios.js
@@ -1,11 +1,12 @@
 // Import a library to help create a Component
 import React from 'react'
 import {Text, AppRegistry} from 'react-native';
+import Header from './src/components/header';
 
 // Create a Component
 
 const App = () => (
-  <Text>Some text!</Text>
+  <Header />
 );
 
 // render it
